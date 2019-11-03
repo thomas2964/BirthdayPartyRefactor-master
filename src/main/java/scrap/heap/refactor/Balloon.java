aa07ff89e1b9620356cf9d593e62d937e2029425
thomas43397 <tom_fok@yahoo.com>
2019-11-02 21:55:05 -0700

    Updated Balloon class

--- src/main/java/scrap/heap/refactor/Balloon.java
+++ src/main/java/scrap/heap/refactor/Balloon.java
@@ -1,4 +1,26 @@
 package scrap.heap.refactor;
 
+enum BalloonColor {
+    RED, BLUE, YELLOW
+}
+
+enum BalloonMaterial {
+    MYLAR, LATEX
+}
+
 public class Balloon {
+
+    private final BalloonColor color;
+    private final BalloonMaterial material;
+    private final int number;
+
+    public Balloon(BalloonColor color, BalloonMaterial material, int number) {
+        this.color = color;
+        this.material = material;
+        this.number = number;
+    }
+
+    void orderBalloon() {
+        System.out.println("Refactored Balloons ordered; " + color + ", " + material  + ", " + number);
+    }
 }
