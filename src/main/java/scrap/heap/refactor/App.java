c00f861fc068e1895471dd2dfb4ec0238b4d663d
thomas2964 <thomasfok@yahoo.com>
2021-01-17 18:47:20 -0800

    Remove some used code

--- src/main/java/scrap/heap/refactor/App.java
+++ src/main/java/scrap/heap/refactor/App.java
@@ -1,9 +1,9 @@
 package scrap.heap.refactor;
 
 public class App {
-    public String getGreeting() {
+/*    public String getGreeting() {
         return "Hello world.";
-    }
+    }*/
 
     public static void main(String[] args) {
 
